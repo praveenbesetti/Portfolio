df549539e5fdd561a6e5cd2ef5df68d8c8d82092
praveenbesetti <praveenbesetti0@gmail.com>
2024-03-19 22:57:23 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,201 @@
+export const Bio = {
+    name: "B S V Praveen",
+    roles: [
+        "Full Stack Developer",
+
+    ],
+    description: "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+    github: "https://github.com/praveenbesetti",
+    resume: "https://drive.google.com/file/d/1rDHkvyI93ceOsyjco0VqhDwjEXm-yEHP/view",
+    linkedin: "https://www.linkedin.com/in/besetti-praveen-691207267/",
+
+    insta: "https://www.instagram.com/kingofgod382/?igsh=MWNhdXVtdWh3aXNsNg%3D%3D",
+
+};
+
+export const skills = [{
+        title: "Frontend",
+        skills: [{
+                name: "React Js",
+                image: "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+            },
+
+            {
+                name: "HTML",
+                image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+            },
+            {
+                name: "CSS",
+                image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+            },
+            {
+                name: "JavaScript",
+                image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+            },
+            {
+                name: "Bootstrap",
+                image: "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+            },
+
+
+        ],
+    },
+    {
+        title: "Backend",
+        skills: [{
+                name: "Java",
+                image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+            },
+            {
+                name: "Python",
+                image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+            },
+            {
+                name: "MySQL",
+                image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+            },
+
+        ],
+    },
+
+    {
+        title: "Others",
+        skills: [{
+                name: "Git",
+                image: "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+            },
+            {
+                name: "GitHub",
+                image: "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+            },
+
+            {
+                name: "Netlify",
+                image: "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+            },
+            {
+                name: "VS Code",
+                image: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+            },
+            {
+                name: "Postman",
+                image: "img/postman.jpg",
+            },
+
+        ],
+    },
+];
+
+
+
+export const education = [{
+        id: 0,
+        img: "img/aditya.jpg",
+        school: "Aditya Degree College, Andhra Pradesh",
+        date: "Oct 2021 - Sep 2024",
+        grade: "8.50 CGPA",
+        desc: "I am currently pursuing a Bachelor's degree in Bachelor Of Computer Application  at Aditya Degree College, Visakhapatnam. I have completed 5 semesters and have a CGPA of 8.50. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Java Full Stack , among others. "
+    },
+    {
+        id: 1,
+        img: "img/surya.jpg",
+        school: "Sri Surya Junior College",
+        date: "Apr 2019 - Apr 2021",
+        grade: "78.5%",
+        desc: "I completed my class 12 high school education at Sri Surya Junior College, Visakhapatnam, where I studied MPC.",
+        degree: "ISC(XII), Science with Computer",
+    },
+    {
+        id: 2,
+        img: "img/chaitanya.jpg",
+        school: "Chaitanya English Medium School",
+        date: "Apr 2018 - Apr 2019",
+        grade: "77.3%",
+        desc: "I completed my class 10 education at Methodist School, Visakhapatnam, where I studied SSC.",
+        degree: "ICSC(X), SSC",
+    },
+];
+
+export const projects = [{
+        id: 9,
+        title: "Car-Rental ",
+        date: "Oct 2023 - Nov 2023",
+        description: "I crafted a visually captivating website dedicated to the passion for driving, seamlessly blending HTML, CSS, and JavaScript to provide users with an interactive platform. With a focus on automotive culture, my design integrates sleek aesthetics and smooth functionality, offering visitors a unique exploration into the world of driven-in-cars.",
+        image: "img/driven.png",
+        tags: [
+            "Html",
+            "Css",
+            "Javascript",
+
+
+        ],
+        category: "web app",
+        github: "https://github.com/rishavchanda/Trackify",
+        webapp: "https://65e9c6db77c69312b5f016be--comfy-sunshine-cf8bca.netlify.app/",
+    },
+    {
+        id: 0,
+        title: "Quiz-Web",
+        date: "Apr 2023 - May 2023",
+        description: "I engineered a captivating quiz website employing HTML, CSS, and JavaScript, seamlessly blending aesthetics with interactivity. Users can enjoy an immersive experience as they navigate through diverse quiz topics, with responsive design ensuring accessibility across devices. This dynamic platform offers both entertainment and educational value, enriching the user experience with engaging content and smooth functionality.",
+        image: "img/quiz.png",
+        tags: ["Html", "Css", "Javascript"],
+        category: "web app",
+        github: "https://github.com/rishavchanda/Podstream",
+        webapp: "https://podstream.netlify.app/",
+
+    },
+    {
+        id: 1,
+        title: "Weather-Web",
+        date: "june 2023 ",
+        description: "I crafted a weather application using HTML, CSS, JavaScript, and the OpenWeatherMap API, providing users with real-time weather information. With a sleek and intuitive interface, users can easily access forecasts for locations worldwide, enhancing their planning and decision-making processes. This dynamic application seamlessly integrates design aesthetics with powerful functionality, offering a seamless and informative weather experience.",
+        image: "img/weather.png",
+        tags: [
+            "Html",
+            "Css",
+            "Javascript",
+            "SMTP Api"
+        ],
+        category: "web app",
+        github: "https://github.com/praveenbesetti/weather-monitoring",
+        webapp: "https://65e9c71559073d0e3f65ae2d--bejewelled-biscuit-41e2bf.netlify.app/",
+    },
+    {
+        id: 2,
+        title: "Car Agency",
+        date: "Feb 2024",
+        description: "I built a robust car agency application using React.js for the frontend and PHP for the backend, leveraging REST APIs for seamless communication. This dynamic platform facilitates smooth browsing and management of car listings, enabling users to explore available inventory and make inquiries effortlessly. With a user-friendly interface and efficient data handling, the application offers a streamlined experience for both clients and administrators in the car dealership industry.",
+        image: "img/login.png",
+        tags: ["Php", "Sql", "Rest Api", "React"],
+        category: "Full Stack",
+        github: "https://github.com/praveenbesetti/React-php",
+        webapp: "https://65f1e10f407d710008e9227f--lucent-chaja-510b31.netlify.app/",
+
+    },
+
+
+    {
+        id: 5,
+        title: "Amazon",
+        date: "Jun 2022",
+        description: " I crafted a static Amazon-inspired webpage using HTML and CSS, capturing the essence of the renowned e-commerce platform's design aesthetic. With meticulously styled elements and responsive layouts, the page mirrors Amazon's professionalism and usability, offering a visually appealing browsing experience for users.",
+        image: "img/Amazon.png",
+        tags: ["Html", "Css"],
+        category: "web app",
+        github: "https://github.com/praveenbesetti/Amazon/upload/main",
+        webapp: "https://65e9c7e94f739014151147ab--tranquil-alfajores-73e5e7.netlify.app/",
+    },
+    {
+        id: 6,
+        title: "User Details",
+        date: "Feb 2024",
+        description: "I created a productivity application using React.js that efficiently stores user details and tracks daily productivity. With intuitive interfaces and state management, users can easily input and access their productivity data, fostering accountability and organization in their daily routines. This dynamic React application empowers users to optimize their productivity levels and achieve their goals effectively.",
+        image: "img/app.png",
+        tags: ["React Js", "API", "Axios", ],
+        category: "React",
+        github: "https://github.com/praveenbesetti/App",
+        webapp: "https://65e4152b619af44ede102f3b--poetic-heliotrope-85fddc.netlify.app/",
+    },
+
+];
